4abab8b044e9fac8da8e2198ad2c88a777c9c31a
Yvan Tankeu <yvantankeu@yahoo.fr>
2022-04-02 11:48:48 -0400

    j'ai retiré la page de login et mettre la  page d'accueil

--- src/main.cpp
+++ src/main.cpp
@@ -45,55 +45,105 @@ RTC_DS3231 rtc;
 
 WebServer server(80);
 
-/*
- *  Page de login
- */
-const char* loginIndex = 
- "<form name='loginForm'>"
-    "<table width='20%' bgcolor='A09F9F' align='center'>"
-        "<tr>"
-            "<td colspan=2>"
-                "<center><font size=4><b>ESP32 Login Page</b></font></center>"
-                "<br>"
-            "</td>"
-            "<br>"
-            "<br>"
-        "</tr>"
-        "<td>Username:</td>"
-        "<td><input type='text' size=25 name='userid'><br></td>"
-        "</tr>"
-        "<br>"
-        "<br>"
-        "<tr>"
-            "<td>Password:</td>"
-            "<td><input type='Password' size=25 name='pwd'><br></td>"
-            "<br>"
-            "<br>"
-        "</tr>"
-        "<tr>"
-            "<td><input type='submit' onclick='check(this.form)' value='Login'></td>"
-        "</tr>"
-    "</table>"
-"</form>"
-"<script>"
-    "function check(form)"
-    "{"
-    "if(form.userid.value=='admin' && form.pwd.value=='admin')"
-    "{"
-    "window.open('/serverIndex')"
+const char* startIndex =
+    "<!doctype html>"
+    "<html lang='en'>"
+
+    "<head>"
+    "<!-- Required meta tags -->"
+    "<meta charset='utf-8'>"
+    "<meta name='viewport' content='width=device-width, initial-scale=1'>"
+
+    "<!-- Bootstrap CSS -->"
+    "<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet'>"
+
+    "<!-- Font awesome ico CSS -->"
+    "<link href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css' rel='stylesheet'>"
+
+    "<style>"
+    ".img-fluid {"
+    "max-width: 70%;"
     "}"
-    "else"
-    "{"
-    " alert('Error Password or Username')/*displays error message*/"
+
+    ".chat {"
+    "max-width: 20%;"
+    "margin-left: 540px;"
+    "margin-top: -100px;"
+    "/* Une ombre noire avec un rayon de flou de 10px */"
+    "filter: drop-shadow(16px 16px 10px rgb(1, 73, 31))"
     "}"
+
+    ".textChat {"
+    "font-size: 25px;"
+    "color: black;"
+    "max-width: 20%;"
+    "margin-left: 550px;"
+    "margin-top: -60px;"
+
+    "/* Une ombre noire avec un rayon de flou de 10px */"
+    "filter: drop-shadow(16px 16px 10px rgb(255, 255, 255))"
     "}"
-"</script>";
- 
+
+    ".info {"
+    "margin-left: 12px;"
+    "height: 180px;"
+    "width: 400px;"
+    "background-color: rgb(255, 255, 255);"
+    "filter: drop-shadow(16px 10px 5px rgb(1, 73, 31))"
+    " }"
+
+    "#barBlanche {"
+    "height: 1px;"
+    "background-color: rgb(0, 0, 0);"
+    "left: 5px;"
+    "bottom: 0.5em;"
+    "position: relative;"
+    "}"
+    "</style>"
+
+    "<title>ma plante</title>"
+    "</head>"
+
+    "<body>"
+    "<div class='container bg-success text-white'>"
+    "<h1 class = 'row justify-content-center text-white  mb-4 shadow-lg p-3 mb-2 rounded'>PLANTE CONNECTE</h1>"
+    "<div class='row'> "
+    "<div class='row mt-5'></div>"
+    "<div class='row mt-5'></div>"
+
+    "<div class='row position-relative'>"
+    "<img src='https://i.ibb.co/5nz5Rsw/chat.png' class='chat  float-end img-fluid position-absolute bottom-90 end-10 ' alt='chat'>"
+    "<span class='textChat'>Je suis rassasié :)</span>"
+    "</div>"
+    "</div>"
+    "<div class='row'>"
+    "<div class='row mb-5'></div>"
+    "<div class='row mb-5'></div>"
+    "</div>"
+    "<div class='row position-relative mt-5'>"
+    "<div class='col position-absolute bottom-25 start-0 info'>"
+    "<h1 class='text-dark'> Mes informations <span class='fa-solid fa-circle-info text-primary h3 text-center'></span></h1>"
+    "<div id='barBlanche'></div>"
+    "<div class='ow text-dark p-3'>"
+    "Je suis actu à 100% d'humidité, il est important que je reste desechée"
+    "</div>"
+    "</div>"
+    "<div class='col'>"
+    "<img src='https://i.ibb.co/WFGtN1L/plante.png' class='float-end img-fluid position-absolute bottom-0 end-0' alt='image png plante'>"
+    "</div>"
+    "<!--JavaScript-->"
+    "<img src='https://i.ibb.co/t4nsJfR/table1.png' id='table' class='img' alt='La table'>"
+    "</div>"
+    "</div>"
+    "</body>"
+
+    "</html>";
+
 /*
  * Server Index Page
  */
  
-const char* serverIndex = 
+const char* updatePage = 
 "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>"
 "<form method='POST' action='#' enctype='multipart/form-data' id='upload_form'>"
    "<input type='file' name='update'>"
@@ -154,7 +204,7 @@ void setup () {
   }
 
   /*
-  * Configuration du server et du wifi
+  * Configuration du server, du wifi et la mise à jour
   */
 
   // Se connecter au reseau wifi
@@ -181,14 +231,14 @@ void setup () {
   }
   Serial.println("Démarrage du répondeur mDNS");
 
-  /* Renvoie la page d'index qui est stockée dans serverIndex */
+  /* Renvoie la page d'index qui est stockée dans updatePage */
   server.on("/", HTTP_GET, []() {
     server.sendHeader("Connection", "fermée");
-    server.send(200, "text/html", loginIndex);
+    server.send(200, "text/html", startIndex);
   });
-  server.on("/serverIndex", HTTP_GET, []() {
+  server.on("/updatePage", HTTP_GET, []() {
     server.sendHeader("Connection", "fermée");
-    server.send(200, "text/html", serverIndex);
+    server.send(200, "text/html", updatePage);
   });
 
   /* Gestion du téléchargement du fichier du firmware */
