7366130d41c57c43b58bc02a98221f18a812680f
Yvan Tankeu <yvantankeu@yahoo.fr>
2022-04-05 08:32:41 -0400

    Projet gere ajax et html

--- src/main.cpp
+++ src/main.cpp
@@ -21,9 +21,8 @@
 #include <WiFi.h>
 #include <WiFiClient.h>
 
-// 03 Includes pour le serveur web, dns et mise à jour
+// 02 Includes pour le serveur web et mise à jour
 #include <WebServer.h>
-#include <ESPmDNS.h>
 #include <Update.h>
 
 // Identifiants du wifi
@@ -32,12 +31,12 @@ const char* ssid = "Galaxi";
 const char* password = "12345678";
 
 const int relaiLED = 33;
-const unsigned long DELAI = 3000;
-const double SEUIL_HUMIDITE = 300.0;
+const unsigned long DELAI = 1000;
+const unsigned int SEUIL_HUMIDITE = 300;
 
 
 //Déclaration de variables
-double soilHumidity = 0.0;
+unsigned int soilHumidity = 0;
 
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday",
                              "Thursday", "Friday", "Saturday"};
@@ -45,104 +44,160 @@ RTC_DS3231 rtc;
 
 WebServer server(80);
 
-const char* startIndex =
-    "<!doctype html>"
-    "<html lang='en'>"
+String actualDate = "";
+String startIndex = R"=====(
+  <!doctype html>
+<html lang="en">
 
-    "<head>"
-    "<!-- Required meta tags -->"
-    "<meta charset='utf-8'>"
-    "<meta name='viewport' content='width=device-width, initial-scale=1'>"
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    "<!-- Bootstrap CSS -->"
-    "<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet'>"
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
-    "<!-- Font awesome ico CSS -->"
-    "<link href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css' rel='stylesheet'>"
+    <!-- Font awesome ico CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
 
-    "<style>"
-    ".img-fluid {"
-    "max-width: 70%;"
-    "}"
+    <style>
+        .img-fluid {
+            max-width: 70%;
+        }
 
-    ".chat {"
-    "max-width: 20%;"
-    "margin-left: 540px;"
-    "margin-top: -100px;"
-    "/* Une ombre noire avec un rayon de flou de 10px */"
-    "filter: drop-shadow(16px 16px 10px rgb(1, 73, 31))"
-    "}"
+        .chat {
+            max-width: 20%;
+            margin-left: 540px;
+            margin-top: -100px;
+            /* Une ombre noire avec un rayon de flou de 10px */
+            filter: drop-shadow(16px 16px 10px rgb(1, 73, 31))
+        }
 
-    ".textChat {"
-    "font-size: 25px;"
-    "color: black;"
-    "max-width: 20%;"
-    "margin-left: 550px;"
-    "margin-top: -60px;"
+        #textChat {
+            font-size: 25px;
+            color: black;
+            max-width: 20%;
+            margin-left: 550px;
+            margin-top: -60px;
 
-    "/* Une ombre noire avec un rayon de flou de 10px */"
-    "filter: drop-shadow(16px 16px 10px rgb(255, 255, 255))"
-    "}"
+            /* Une ombre noire avec un rayon de flou de 10px */
+            filter: drop-shadow(16px 16px 10px rgb(255, 255, 255))
+        }
 
-    ".info {"
-    "margin-left: 12px;"
-    "height: 180px;"
-    "width: 400px;"
-    "background-color: rgb(255, 255, 255);"
-    "filter: drop-shadow(16px 10px 5px rgb(1, 73, 31))"
-    " }"
+        .info {
+            margin-left: 12px;
+            height: 180px;
+            width: 400px;
+            background-color: rgb(255, 255, 255);
+            filter: drop-shadow(16px 10px 5px rgb(1, 73, 31))
+        }
 
-    "#barBlanche {"
-    "height: 1px;"
-    "background-color: rgb(0, 0, 0);"
-    "left: 5px;"
-    "bottom: 0.5em;"
-    "position: relative;"
-    "}"
-    "</style>"
+        #barBlanche {
+            height: 1px;
+            background-color: rgb(0, 0, 0);
+            left: 5px;
+            bottom: 0.5em;
+            position: relative;
+        }
+    </style>
 
-    "<title>ma plante</title>"
-    "</head>"
+    <title>ma plante</title>
+</head>
 
-    "<body>"
-    "<div class='container bg-success text-white'>"
-    "<h1 class = 'row justify-content-center text-white  mb-4 shadow-lg p-3 mb-2 rounded'>PLANTE CONNECTE</h1>"
-    "<div class='row'> "
-    "<div class='row mt-5'></div>"
-    "<div class='row mt-5'></div>"
+<!-------------------------HTML------------------------->
+<body>
+    <div class="container bg-success text-white">
+        <h1 class="row justify-content-center text-white  mb-4 shadow-lg p-3 mb-2 rounded">PLANTE CONNECTEE <span
+                class="text-center h6" style="display: inline;" id="date" 0></span></h1>
+        <div class="row">
+            <div class="row mt-5"></div>
+            <div class="row mt-5"></div>
 
-    "<div class='row position-relative'>"
-    "<img src='https://i.ibb.co/5nz5Rsw/chat.png' class='chat  float-end img-fluid position-absolute bottom-90 end-10 ' alt='chat'>"
-    "<span class='textChat'>Je suis rassasié :)</span>"
-    "</div>"
-    "</div>"
-    "<div class='row'>"
-    "<div class='row mb-5'></div>"
-    "<div class='row mb-5'></div>"
-    "</div>"
-    "<div class='row position-relative mt-5'>"
-    "<div class='col position-absolute bottom-25 start-0 info'>"
-    "<h1 class='text-dark'> Mes informations <span class='fa-solid fa-circle-info text-primary h3 text-center'></span></h1>"
-    "<div id='barBlanche'></div>"
-    "<div class='ow text-dark p-3'>"
-    "Je suis actu à 100% d'humidité, il est important que je reste desechée"
-    "</div>"
-    "</div>"
-    "<div class='col'>"
-    "<img src='https://i.ibb.co/WFGtN1L/plante.png' class='float-end img-fluid position-absolute bottom-0 end-0' alt='image png plante'>"
-    "</div>"
-    "<!--JavaScript-->"
-    "<img src='https://i.ibb.co/t4nsJfR/table1.png' id='table' class='img' alt='La table'>"
-    "</div>"
-    "</div>"
-    "</body>"
 
-    "</html>";
+            <div class="row position-relative ">
+                <img src="https://i.ibb.co/5nz5Rsw/chat.png"
+                    class="chat  float-end img-fluid position-absolute bottom-90 end-10 " alt="chat">
+                <span id="textChat">Je suis rassasiée! :)</span>
+            </div>
+        </div>
+        <div class="row ">
+            <div class="row mb-5"></div>
+            <div class="row mb-5"></div>
+        </div>
+        <div class="row position-relative mt-5">
+            <div class="col position-absolute bottom-25 start-0 info">
+                <h1 class="text-dark"> Mes informations <span
+                        class="fa-solid fa-circle-info text-primary h3 text-center "></span></h1>
+                <div id="barBlanche"></div>
+                <div class="row text-dark p-3" style="display: inline;">
+                    Je suis actu à <span class="text-primary" id="soilHumidityVal">0</span> % d'humidité, il est
+                    important que je reste desechée
+                </div>
+            </div>
+            <div class="col ">
+                <img src="https://i.ibb.co/WFGtN1L/plante.png"
+                    class="float-end img-fluid position-absolute bottom-0 end-0 " alt="image png plante">
+            </div>
+            <!--JavaScript-->
+            <img src="https://i.ibb.co/t4nsJfR/table1.png" id="table" class="img" alt="La table">
+        </div>
+    </div>
+</body>
+
+<!-------------------------JavaScript------------------------->
+<script>
+
+    //Fonction qui va s'executer chaque 2 secondes
+    setInterval(function () {
+        getSoilHumidityVal(); // on appel la fonction qui nous permet d'avoir la valeur d'humidite
+    }, 2000);
+
+   /* window.onload = function () {
+      msgPlante();
+    }*/
+
+    // Requete ajax pour la mise à jour de la valeur d'humidité
+    function getSoilHumidityVal() {
+        var SoilValRequest = new XMLHttpRequest();
+        SoilValRequest.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("soilHumidityVal").innerHTML =
+                    this.responseText;
+            }
+        };
+        SoilValRequest.open("GET", "/readHumVal", true);
+        SoilValRequest.send();
+    }
+
+    // Requete ajax pour la mise à jour de la date
+    setInterval(function () {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("date").innerHTML = this.responseText;
+            }
+        };
+        xhttp.open("GET", "/dateValue", true);
+        xhttp.send();
+    }, 60000);
+
+    // Message de la plante
+     setInterval(function () {
+        let soilHumVal = document.getElementById("soilHumidityVal").innerHTML;
+        soilHumVal = parseFloat(soilHumVal, 10);
+        let msgPlanteValue = document.getElementById("textChat");
+
+        if (soilHumVal <= 300) {
+            msgPlanteValue.innerHTML = "Qu'on m'apporte à boire! :(";
+        }
+    }, 2000);
+
+</script>
+</html>)=====";
 
 /*
- * Server Index Page
+ * Server update Page
  */
- 
 const char* updatePage = 
 "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>"
 "<form method='POST' action='#' enctype='multipart/form-data' id='upload_form'>"
@@ -222,28 +277,35 @@ void setup () {
   Serial.print("Address IP: ");
   Serial.println(WiFi.localIP());
 
-  /* utilisation du mdns pour la résolution des noms d'hôtes */
-  if (!MDNS.begin(host)) { //http://esp32.local
-    Serial.println("Erreur dans la configuration du répondeur MDNS !");
-    while (1) {             
-      delay(1000);
-    }
-  }
-  Serial.println("Démarrage du répondeur mDNS");
-
-  /* Renvoie la page d'index qui est stockée dans updatePage */
+  // Renvoie la page d'acceuil
   server.on("/", HTTP_GET, []() {
-    server.sendHeader("Connection", "fermée");
+    server.sendHeader("Connection", "close");
+    server.setContentLength(startIndex.length());
     server.send(200, "text/html", startIndex);
+    server.setContentLength(startIndex.length());
   });
+
+  // requete ajax pour la valeur de l'humidité
+  server.on("/readHumVal", HTTP_GET, []() {
+    String humide = String(soilHumidity);
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/plain", humide);
+  });
+
+  // requete ajax pour la valeur de l'humidité
+  server.on("/dateValue", HTTP_GET, []() {
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/plain", actualDate);
+  });
+  
   server.on("/updatePage", HTTP_GET, []() {
-    server.sendHeader("Connection", "fermée");
+    server.sendHeader("Connection", "close");
     server.send(200, "text/html", updatePage);
   });
 
   /* Gestion du téléchargement du fichier du firmware */
   server.on("/update", HTTP_POST, []() {
-    server.sendHeader("Connection", "fermée");
+    server.sendHeader("Connection", "close");
     server.send(200, "text/plain", (Update.hasError()) ? "ECHEC" : "OK");
     ESP.restart();
   }, []() {
@@ -268,56 +330,44 @@ void setup () {
     }
   });
   server.begin();
+  Serial.println("HTTP server started");
 }
 
 void loop () {
 
   server.handleClient();
-  delay(1);
+  //delay(1);
 
   // Code valeur du capteur d'humidité du sol
   soilHumidity = analogRead(A0);
   Serial.print("Humidité du sol: ");
   Serial.println(soilHumidity);
 
+  // La plante se fait arroser automatiquement
   if (soilHumidity <= SEUIL_HUMIDITE){
-    //Serial.println("val inferieur à 301");
     Serial.println("Sol sec, plante doit être arrosée");
-  Serial.println();
-    digitalWrite(relaiLED, HIGH);
+    Serial.println();
+    //digitalWrite(relaiLED, HIGH); 
   }
-  else if (soilHumidity > SEUIL_HUMIDITE)
+  if (soilHumidity > SEUIL_HUMIDITE)
   {
-    //Serial.println("val superieur à 300");
     Serial.println("Sol humide, bien pour la plante");
     Serial.println();
-
-    digitalWrite(relaiLED, LOW);
+    //digitalWrite(relaiLED, LOW);
   }
 
-/*  // Code module rtc
+/// Code module rtc
   DateTime now = rtc.now();
 
-  Serial.print(now.year(), DEC);
-  Serial.print('/');
-  Serial.print(now.month(), DEC);
-  Serial.print('/');
-  Serial.print(now.day(), DEC);
-  Serial.print(" (");
-  Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
-  Serial.print(") ");
-  Serial.print(now.hour()+1, DEC);
-  Serial.print(':');
-  Serial.print(now.minute()+5, DEC);
-  Serial.print(':');
-  Serial.print(now.second(), DEC);
-  Serial.println();
+  actualDate =  String(now.year(), DEC) + "/" 
+                + String(now.month(), DEC) + "/" 
+                +String(now.day(), DEC) + "(" 
+                + String(daysOfTheWeek[now.dayOfTheWeek()]) + ")   Heure : " 
+                +String(now.hour() +1 , DEC) + "H:" 
+                + String(now.minute() + 4, DEC) + " min" ;
 
-  
-  delay(delayTime);
-  digitalWrite(relaiLED, HIGH);
-  delay(delayTime);
-  digitalWrite(relaiLED, LOW);*/
+  //Serial.println(actualDate);
+
   delay(DELAI);
 
 }
